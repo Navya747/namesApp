bc86cfa458f2fbe8d5c132e362446c14c6d1c614
Navya747 <navyamallela747@gmail.com>
2020-02-26 18:01:25 +0500

    Updates

---
+++ precache-manifest.73f7e6e1301ef6926797d5e699b61921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24dc06592dc96a384fa0acd5b744102b",
+    "url": "/namesApp/index.html"
+  },
+  {
+    "revision": "3771c27d74baf565b1ba",
+    "url": "/namesApp/static/css/main.c4c46d4f.chunk.css"
+  },
+  {
+    "revision": "1c7c113d43aa56cd890c",
+    "url": "/namesApp/static/js/2.5dbf2bd0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/namesApp/static/js/2.5dbf2bd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3771c27d74baf565b1ba",
+    "url": "/namesApp/static/js/main.272fe4d8.chunk.js"
+  },
+  {
+    "revision": "2b85d1694c743c666872",
+    "url": "/namesApp/static/js/runtime-main.b9733be3.js"
+  }
+]);
